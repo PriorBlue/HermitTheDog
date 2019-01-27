2463f4d99e2aeaa43e0981991ecae1742bf3a5ba
Tom <mail@tomgon.de>
2019-01-27 14:27:47 +0100

    light cookie + script

---
+++ HermitTheDog/Assets/Scripts/LightMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMove : MonoBehaviour
+{
+	
+	public Transform Light;
+	public Vector3 Position = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Light.position += Position * Time.deltaTime;
+    }
+}
